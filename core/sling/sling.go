5543645dbdc65a0a4eb70c7595a99b985aba6a4d
Fritz Larco <flarco@live.com>
2021-07-24 10:10:07 -0300

    [skip ci] update Sling method

--- core/sling/sling.go
+++ core/sling/sling.go
@@ -9,11 +9,6 @@ import (
 // Sling accepts a configuration and runs an Extract-Load task
 func Sling(cfg Config) (err error) {
 
-	err = cfg.Prepare()
-	if err != nil {
-		return g.Error(err, "unable to accept Sling config")
-	}
-
 	task := NewTask(int(time.Now().Unix()), cfg)
 	if task.Err != nil {
 		return g.Error(task.Err, "error creating Sling task")
