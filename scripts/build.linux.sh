d075ab00ab539e4bfd2264579c9ecda30c0a987b
Fritz Larco <flarco@live.com>
2021-10-23 18:56:12 -0300

    fix build.linux.sh

--- scripts/build.linux.sh
+++ scripts/build.linux.sh
@@ -8,7 +8,6 @@ rm -rf .git
 
 mkdir python/sling/bin
 GOOS=linux GOARCH=amd64 go build -o sling-linux cmd/sling/*.go
-d run -it -v $(pwd):/work -w /work golang:1.16-stretch go build -o sling-linux cmd/sling/*.go
 mv -f sling-linux python/sling/bin/
 
 rm -rf /tmp/sling
