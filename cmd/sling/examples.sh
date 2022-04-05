f21095bb43dd4b6531b62b510ceb5f4aea60a41a
Fritz Larco <flarco@live.com>
2022-04-05 08:13:53 -0300

    update examples

--- cmd/sling/examples.sh
+++ cmd/sling/examples.sh
@@ -1,46 +1,46 @@
 
 ###### Database to Database ######
-# drop target table
-export PG_DB_URL=postgres://xxxxxxxxxxxxxxxx
-export MYSQL_DB_URL=mysql://xxxxxxxxxxxxxxxx
+# drop / full refresh target table
+export PG_DB=postgres://xxxxxxxxxxxxxxxx
+export MYSQL_DB=mysql://xxxxxxxxxxxxxxxx
 
-sling run --src-conn PG_DB_URL --src-stream public.transactions --tgt-conn MYSQL_DB_URL  --tgt-object mysql.bank_transactions --mode drop
+sling run --src-conn PG_DB --src-stream public.transactions --tgt-conn MYSQL_DB  --tgt-object mysql.bank_transactions --mode drop
 # OR
 sling run -c '
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: public.transactions
 target:
-  conn: MYSQL_DB_URL
+  conn: MYSQL_DB
   object: mysql.bank_transactions
   mode: drop
 '
 
 # custom sql in-line
-export PG_DB_URL=$POSTGRES_URL
-export MYSQL_DB_URL=$MYSQL_URL
+export PG_DB=$POSTGRES_URL
+export MYSQL_DB=$MYSQL_URL
 
-sling run --src-conn PG_DB_URL --src-stream "select date, description, amount from public.transactions where transaction_type = 'debit'" --tgt-conn MYSQL_DB_URL  --tgt-object mysql.bank_transactions --mode drop
+sling run --src-conn PG_DB --src-stream "select date, description, amount from public.transactions where transaction_type = 'debit'" --tgt-conn MYSQL_DB  --tgt-object mysql.bank_transactions --mode drop
 # OR
 sling run -c "
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: select date, description, amount from public.transactions where transaction_type = 'debit'
 target:
-  conn: MYSQL_DB_URL
+  conn: MYSQL_DB
   object: mysql.bank_transactions
   mode: drop
 "
 
 # custom sql file
-sling run --src-conn PG_DB_URL --src-stream file:///path/to/query.sql --tgt-conn MYSQL_DB_URL  --tgt-object mysql.bank_transactions --mode append
+sling run --src-conn PG_DB --src-stream file:///path/to/query.sql --tgt-conn MYSQL_DB  --tgt-object mysql.bank_transactions --mode append
 # OR
 sling run -c '
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: file:///path/to/query.sql
 target:
-  conn: MYSQL_DB_URL
+  conn: MYSQL_DB
   object: mysql.bank_transactions
   mode: append
 '
@@ -48,10 +48,10 @@ target:
 # upsert
 sling run -c '
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: public.transactions
 target:
-  conn: MYSQL_DB_URL
+  conn: MYSQL_DB
   object: mysql.bank_transactions
   mode: upsert
   update_key: modified_at
@@ -62,11 +62,11 @@ target:
 ###### Database to File ######
 # CSV export full table
 
-sling run --src-conn PG_DB_URL --src-stream public.transactions --tgt-object file:///tmp/public.transactions.csv
+sling run --src-conn PG_DB --src-stream public.transactions --tgt-object file:///tmp/public.transactions.csv
 # OR
 sling run -c '
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: public.transactions
 target:
   object: file:///tmp/public.transactions.csv
@@ -75,7 +75,7 @@ target:
 # CSV dump, custom SQL
 sling run -c "
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: select id, created_at, account_id, amount from public.transactions where type = 'A'
 target:  
   object: file:///tmp/public.transactions.csv
@@ -84,11 +84,11 @@ target:
 # CSV export full table to S3, gzip
 export AWS_ACCESS_KEY_ID=xxxxxxxxxxxxx
 export AWS_SECRET_ACCESS_KEY=xxxxxxxxx
-sling run --src-conn PG_DB_URL --src-stream public.transactions --tgt-object file:///tmp/public.transactions.csv --tgt-options 'compression: gzip'
+sling run --src-conn PG_DB --src-stream public.transactions --tgt-object file:///tmp/public.transactions.csv --tgt-options 'compression: gzip'
 # OR
 sling run -c '
 source:
-  conn: PG_DB_URL
+  conn: PG_DB
   stream: public.transactions
 target:  
   object: s3://my-bucket/public.transactions.csv.gz
@@ -98,15 +98,15 @@ target:
 
 ###### File to Database ######
 # local CSV import into table
-cat /tmp/public.transactions.csv.gz | sling run --tgt-conn PG_DB_URL --tgt-object public.transactions
+cat /tmp/public.transactions.csv.gz | sling run --tgt-conn PG_DB --tgt-object public.transactions
 # OR
-sling run --src-stream file:///tmp/public.transactions.csv.gz --tgt-conn PG_DB_URL --tgt-object public.transactions
+sling run --src-stream file:///tmp/public.transactions.csv.gz --tgt-conn PG_DB --tgt-object public.transactions
 # OR
 sling run -c '
 source:
   stream: file:///tmp/public.transactions.csv.gz
 target:
-  conn: PG_DB_URL
+  conn: PG_DB
   object: public.transactions
   mode: append
 '
@@ -119,7 +119,7 @@ sling run -c '
 source:
   stream: s3://my-bucket/public.transactions/
 target:
-  conn: PG_DB_URL
+  conn: PG_DB
   object: public.transactions
   mode: upsert
   update_key: modified_at
