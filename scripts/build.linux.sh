400716c18f698b2da46e8a34a1eb39106893d1a6
Fritz Larco <flarco@live.com>
2022-07-22 10:16:07 -0300

    update versioning [no test]

--- scripts/build.linux.sh
+++ scripts/build.linux.sh
@@ -6,7 +6,7 @@ cp -r . /tmp/sling
 cd /tmp/sling
 rm -rf .git
 
-echo "GIT_TAG=`echo $(git describe --tags --abbrev=0)`" >> $VERSION
+
 echo "VERSION -> $VERSION"
 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'core.Version=$VERSION'" -o sling-linux cmd/sling/*.go
 
