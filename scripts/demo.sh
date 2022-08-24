63a773c72d62485816c1fd7c723c4c62cc7c0a1c
Fritz Larco <flarco@live.com>
2022-08-24 11:09:20 -0300

    update demo [skip ci]

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,12 +1,17 @@
 
-terminalizer init # ~/.terminalizer/config.yml
 sling run --src-conn PG_BIONIC --src-stream crypto.ccompare_price_day --tgt-conn POSTGRES --tgt-object public.ccompare_price_day
 
-cd /tmp
-terminalizer record demo
+pip install asciinema
+asciinema rec
 export MY_PG_DB='postgresql://postgres:postgres@bionic:55432/postgres?sslmode=disable'
 sling run --src-conn MY_PG_DB --src-stream public.ccompare_price_day --stdout > my_file.csv
 gzip my_file.csv
 cat my_file.csv.gz | sling run --tgt-conn MY_PG_DB --tgt-object public.my_table --mode full-refresh
 
-mv demo.yml /__/devbox/sling-cli/
+mv demo.cast /__/devbox/sling-cli/
+
+asciinema upload demo.cast
+
+# generating GIF
+brew install gifsicle
+asciicast2gif https://asciinema.org/a/tUNedIII3iE5oFwh6ycWZVjDT.json demo.gif
