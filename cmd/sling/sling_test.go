3aecf15981800bc98e2eb4ba03b6b914acc6fa43
Fritz Larco <flarco@live.com>
2023-02-10 10:07:13 -0300

    improve df.Pause, schemaChgChan [no test]

--- cmd/sling/sling_test.go
+++ cmd/sling/sling_test.go
@@ -544,7 +544,7 @@ options:
 	}
 }
 
-func testOneReplication(t *testing.T) {
+func TestOneReplication(t *testing.T) {
 	os.Setenv("SLING_CLI", "TRUE")
 	os.Setenv("SLING_LOADED_AT_COLUMN", "TRUE")
 	os.Setenv("CONCURENCY_LIMIT", "2")
