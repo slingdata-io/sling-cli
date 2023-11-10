b637de49770ebdc2622022b471dd5e167679fe32
Fritz Larco <flarco@live.com>
2023-11-10 08:25:34 -0300

    update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,11 +18,21 @@ go build -o sling-linux cmd/sling/*.go && chmod +x sling-linux
 ./sling-linux run
 
 cat cmd/sling/tests/files/test1.1.csv | ./sling-linux run --tgt-conn POSTGRES --tgt-object public.my_table --mode full-refresh
+./sling-linux run --src-stream file://cmd/sling/tests/files/test1.1.csv --tgt-conn POSTGRES --tgt-object public.my_table --mode full-refresh
+./sling-linux run --src-stream file://cmd/sling/tests/files/test1.1.csv --tgt-conn MSSQL --tgt-object dbo.my_table --mode full-refresh --tgt-options 'use_bulk: false'
+
 cat cmd/sling/tests/files/test1.1.csv.gz | ./sling-linux run --tgt-conn POSTGRES --tgt-object public.my_table --mode full-refresh
+./sling-linux run --src-stream 'file://cmd/sling/tests/files/test1.1.csv.gz' --tgt-conn MYSQL --tgt-object mysql.my_table --mode full-refresh --tgt-options 'use_bulk: false'
+
 cat cmd/sling/tests/files/test3.json | ./sling-linux run --src-options "flatten: true" --tgt-conn POSTGRES --tgt-object public.my_table1 --tgt-options 'use_bulk: false' --mode full-refresh
+./sling-linux run --src-stream 'file://cmd/sling/tests/files/test3.json'  --src-options "flatten: true" --tgt-conn POSTGRES --tgt-object public.my_table1 --tgt-options 'use_bulk: false' --mode full-refresh
+
+
 ./sling-linux run --src-conn POSTGRES --src-stream public.my_table --stdout > /tmp/my_table.csv
 ./sling-linux run --src-conn POSTGRES --src-stream public.my_table --tgt-object file:///tmp/my_table.csv
 ./sling-linux run -r cmd/sling/tests/replications/r.05.yaml
 ./sling-linux run -r cmd/sling/tests/replications/r.06.yaml
+
 ./sling-linux run --src-stream 'file://cmd/sling/tests/files/parquet' --stdout > /dev/null
+
 echo '' | ./sling-linux run --stdout
