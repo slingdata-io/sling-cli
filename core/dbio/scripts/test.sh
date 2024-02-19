f52eba1a8ccaf13f0fec58c6f0f43632995332fc
Fritz Larco <flarco@live.com>
2024-02-19 16:51:03 -0300

    include dbio tests

--- core/dbio/scripts/test.sh
+++ core/dbio/scripts/test.sh
@@ -11,11 +11,11 @@ cd iop
 go test -run 'TestParseDate|TestDetectDelimiter'
 cd -
 
-cd database
-go test -run 'TestPostgres|TestMySQL|TestOracle|TestSnowflake|TestSqlServer|TestBigQuery|TestSQLite|TestClickhouse' -timeout 10m
-ALLOW_BULK_IMPORT=FALSE go test -run 'TestPostgres|TestMySQL|TestOracle|TestSqlServer|TestSQLite|TestClickhouse' -timeout 10m # test without bulk loading, using transaction batch
-go test -run TestLargeDataset
-cd -
+# cd database
+# go test -run 'TestPostgres|TestMySQL|TestOracle|TestSnowflake|TestSqlServer|TestBigQuery|TestSQLite|TestClickhouse' -timeout 10m
+# ALLOW_BULK_IMPORT=FALSE go test -run 'TestPostgres|TestMySQL|TestOracle|TestSqlServer|TestSQLite|TestClickhouse' -timeout 10m # test without bulk loading, using transaction batch
+# go test -run TestLargeDataset
+# cd -
 
 cd filesys
 go test -run 'TestFileSysLocalCsv|TestFileSysLocalJson|TestFileSysLocalParquet|TestFileSysLocalFormat|TestFileSysGoogle|TestFileSysS3|TestFileSysAzure|TestFileSysSftp|TestExcel'
