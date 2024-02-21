d8578606b3a8df70125d82b57850622126f63346
Fritz Larco <flarco@live.com>
2024-02-21 07:33:46 -0300

    improve IsNaN logic

--- core/dbio/iop/stream_processor.go
+++ core/dbio/iop/stream_processor.go
@@ -528,7 +528,11 @@ func (sp *StreamProcessor) CastVal(i int, val interface{}, col *Column) interfac
 		nVal = iVal
 	case col.Type.IsNumber():
 		fVal, err := sp.toFloat64E(val)
-		if err != nil || math.IsNaN(fVal) {
+		if err == nil && math.IsNaN(fVal) {
+			// set as null
+			cs.NullCnt++
+			return nil
+		} else if err != nil {
 			// is string
 			sp.ds.ChangeColumn(i, StringType)
 			cs.StringCnt++
