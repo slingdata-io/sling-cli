12e29eccf67b44e3b9104bbdbc4115799d4da064
Fritz Larco <flarco@live.com>
2024-02-25 16:42:51 +0000

    fix expand variables for custom SQL

--- core/sling/task_run_read.go
+++ core/sling/task_run_read.go
@@ -27,6 +27,14 @@ func (t *TaskExecution) ReadFromDB(cfg *Config, srcConn database.Connection) (df
 		sTable.Schema = cast.ToString(cfg.Source.Data["schema"])
 	}
 
+	// expand variables for custom SQL
+	fMap, err := t.Config.GetFormatMap()
+	if err != nil {
+		err = g.Error(err, "could not get format map for pre-sql")
+		return t.df, err
+	}
+	sTable.SQL = g.Rm(sTable.SQL, fMap)
+
 	// check if referring to a SQL file
 	if connection.SchemeType(cfg.Source.Stream).IsFile() && g.PathExists(strings.TrimPrefix(cfg.Source.Stream, "file://")) {
 		// for incremental, need to put `{incremental_where_cond}` for proper selecting
@@ -197,13 +205,6 @@ func (t *TaskExecution) ReadFromDB(cfg *Config, srcConn database.Connection) (df
 		srcConn.SetProp("start_time", t.Config.IncrementalVal)
 	}
 
-	// expand variables for custom SQL
-	fMap, err := t.Config.GetFormatMap()
-	if err != nil {
-		err = g.Error(err, "could not get format map for pre-sql")
-		return t.df, err
-	}
-	sTable.SQL = g.Rm(sTable.SQL, fMap)
 	sTable.SQL = g.R(sTable.SQL, "incremental_where_cond", "1=1") // if running non-incremental mode
 
 	// construct SELECT statement for selected fields
