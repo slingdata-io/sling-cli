b4c7badf20397ce7ae40521789b0da5d8548a9e2
Fritz Larco <flarco@live.com>
2024-03-11 21:01:17 -0300

    add md5 transform

--- core/sling/transforms.go
+++ core/sling/transforms.go
@@ -30,6 +30,7 @@ var transforms = map[string]iop.TransformFunc{
 	"decode_latin9":         func(sp *iop.StreamProcessor, val string) (string, error) { return Decode(sp, decISO8859_15, val) },
 	"decode_windows1250":    func(sp *iop.StreamProcessor, val string) (string, error) { return Decode(sp, decWindows1250, val) },
 	"decode_windows1252":    func(sp *iop.StreamProcessor, val string) (string, error) { return Decode(sp, decWindows1252, val) },
+	"md5":                   func(sp *iop.StreamProcessor, val string) (string, error) { return g.MD5(val), nil },
 }
 
 func init() {
