742be65ca63b00bb97380c350ba7545421568059
Fritz Larco <flarco@live.com>
2024-04-04 06:51:43 -0300

    add use database

--- core/dbio/database/database_snowflake.go
+++ core/dbio/database/database_snowflake.go
@@ -117,11 +117,14 @@ func (conn *SnowflakeConn) Connect(timeOut ...int) error {
 		conn.SetProp("warehouse", cast.ToString(data.Rows[0][0]))
 	}
 
-	if conn.GetProp("schema") != "" {
-		_, err = conn.Exec("USE SCHEMA " + conn.GetProp("schema") + noDebugKey)
+	if val := conn.GetProp("database"); val != "" {
+		_, err = conn.Exec("USE DATABASE " + val + noDebugKey)
 	}
-	if conn.GetProp("role") != "" {
-		_, err = conn.Exec("USE ROLE " + conn.GetProp("role") + noDebugKey)
+	if val := conn.GetProp("schema"); val != "" {
+		_, err = conn.Exec("USE SCHEMA " + val + noDebugKey)
+	}
+	if val := conn.GetProp("role"); val != "" {
+		_, err = conn.Exec("USE ROLE " + val + noDebugKey)
 	}
 	return err
 }
