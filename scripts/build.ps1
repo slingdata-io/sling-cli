aac72810f2bdd58a87d2a9b718b82cda8567d70c
Fritz Larco <flarco@live.com>
2024-04-28 14:51:14 -0300

    update build scripts [skip ci]

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,7 @@
 
 go mod edit -dropreplace='github.com/flarco/g' go.mod
 go mod edit -dropreplace='github.com/slingdata-io/sling' go.mod
+go mod edit -droprequire='github.com/slingdata-io/sling' go.mod
 go mod tidy
 
 go build -o sling.exe github.com/slingdata-io/sling-cli/cmd/sling
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,8 @@
 
 go mod edit -dropreplace='github.com/flarco/g' go.mod
 go mod edit -dropreplace='github.com/slingdata-io/sling' go.mod
+go mod edit -droprequire='github.com/slingdata-io/sling' go.mod
+
 go mod tidy
 
 go build -o sling cmd/sling/*.go
