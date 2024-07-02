6002868c32212f6ef3ea0b3df6bef0a8ce7b7179
Fritz Larco <flarco@live.com>
2024-07-02 08:18:19 -0300

    improve FormatTargetObjectName

--- core/sling/config.go
+++ core/sling/config.go
@@ -632,6 +632,11 @@ func (cfg *Config) FormatTargetObjectName() (err error) {
 		return g.Error(err, "could not get formatting variables")
 	}
 
+	// clean values for replacing
+	for k, v := range m {
+		m[k] = iop.CleanName(cast.ToString(v))
+	}
+
 	// replace placeholders
 	cfg.Target.Object = strings.TrimSpace(g.Rm(cfg.Target.Object, m))
 
@@ -696,10 +701,10 @@ func (cfg *Config) GetFormatMap() (m map[string]any, err error) {
 			}
 		}
 		if table.Schema != "" {
-			m["stream_schema"] = iop.CleanName(strings.ToLower(table.Schema))
+			m["stream_schema"] = table.Schema
 		}
 		if table.Name != "" {
-			m["stream_table"] = iop.CleanName(strings.ToLower(table.Name))
+			m["stream_table"] = table.Name
 		}
 
 		if cfg.StreamName != "" {
