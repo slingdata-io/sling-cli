2c485b2336e0efd8a13a71d2ebfa6e1dd06e77cb
Fritz Larco <flarco@live.com>
2024-08-25 08:11:14 -0300

    add panic recovery in parquet

--- core/sling/task.go
+++ core/sling/task.go
@@ -487,6 +487,8 @@ func ErrorHelper(err error) (helpString string) {
 			helpString = "Perhaps setting the delimiter (source_options.delimiter) would help? See https://docs.slingdata.io/sling-cli/run/configuration#source"
 		case contains("not implemented makeGoLangScanType"):
 			helpString = "This is related to the Microsoft go-mssqldb driver, which willingly calls a panic for certain column types (such as geometry columns). See https://github.com/microsoft/go-mssqldb/issues/79 and https://github.com/microsoft/go-mssqldb/pull/32. The workaround is to use Custom SQL, and convert the problematic column type into a varchar."
+		case contains("cannot create parquet value") && contains("from go value of type"):
+			helpString = "This is happening when the column type changes mid-stream. Try casting the problematic column to a proper type with the `columns` property."
 		}
 	}
 	return
