1a5704e71e8e292695f0ae715cc05a7d07ae57e5
Fritz Larco <flarco@live.com>
2024-09-01 13:46:19 -0300

    update tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,6 @@ shopt -s expand_aliases
 
 # sling
 
-# export _DEBUG=LOW
-# export _DEBUG_CALLER_LEVEL=2
 cd cmd/sling
 go test -v -run 'TestReplicationDefaults'
 go test -v -parallel 3 -run 'TestSuiteFile|TestSuiteDatabaseClickhouse'
@@ -18,8 +16,4 @@ go test -run 'TestCheck'
 cd -
 
 ## test cli commands
-go build -o sling-linux cmd/sling/*.go && chmod +x sling-linux
-./sling-linux --version
-alias sling="./sling-linux"
-
-SLING_BIN=./sling-linux go test -v -run TestCLI
+SLING_BIN=./sling go test -v -run TestCLI
