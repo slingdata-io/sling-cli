0526ed38b687887f3c45cbcffe308d9eee1060f2
Fritz Larco <flarco@live.com>
2025-06-18 09:51:02 -0300

    fix: Fully qualify source and target table names in DELETE where expressions for upsert SQL

--- core/dbio/database/database_bigquery.go
+++ core/dbio/database/database_bigquery.go
@@ -1052,11 +1052,25 @@ func (conn *BigQueryConn) GenerateUpsertSQL(srcTable string, tgtTable string, pk
 		return
 	}
 
+	st, _ := ParseTableName(srcTable, conn.Type)
+	tt, _ := ParseTableName(tgtTable, conn.Type)
+
+	// fully qualify src. and tgt. in DELETE where expressions
+	// see https://github.com/slingdata-io/sling-cli/issues/575
+	upsertMap["src_tgt_pk_equal"] = strings.ReplaceAll(
+		upsertMap["src_tgt_pk_equal"],
+		"src.", conn.Quote(st.Name)+".",
+	)
+	upsertMap["src_tgt_pk_equal"] = strings.ReplaceAll(
+		upsertMap["src_tgt_pk_equal"],
+		"tgt.", conn.Quote(tt.Name)+".",
+	)
+
 	sqlTempl := `
-	delete from {tgt_table} tgt
+	delete from {tgt_table}
 	where exists (
 			select 1
-			from {src_table} src
+			from {src_table}
 			where {src_tgt_pk_equal}
 	)
 	;
