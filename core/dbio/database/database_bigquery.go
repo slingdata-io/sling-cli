608ea0fb0841ba481c5cf9ac6526fe7fffe4c754
Fritz Larco <flarco@live.com>
2025-06-21 21:56:21 -0300

    fix(database): add project prefix to source table in GenerateUpsertSQL for correct referencing

--- core/dbio/database/database_bigquery.go
+++ core/dbio/database/database_bigquery.go
@@ -1052,25 +1052,18 @@ func (conn *BigQueryConn) GenerateUpsertSQL(srcTable string, tgtTable string, pk
 		return
 	}
 
-	// st, _ := ParseTableName(srcTable, conn.Type)
-	// tt, _ := ParseTableName(tgtTable, conn.Type)
-
-	// // fully qualify src. and tgt. in DELETE where expressions
-	// // see https://github.com/slingdata-io/sling-cli/issues/575
-	// upsertMap["src_tgt_pk_equal"] = strings.ReplaceAll(
-	// 	upsertMap["src_tgt_pk_equal"],
-	// 	"src.", conn.Quote(st.Name)+".",
-	// )
-	// upsertMap["src_tgt_pk_equal"] = strings.ReplaceAll(
-	// 	upsertMap["src_tgt_pk_equal"],
-	// 	"tgt.", conn.Quote(tt.Name)+".",
-	// )
+	// fully qualify src. in DELETE where expressions
+	// see https://github.com/slingdata-io/sling-cli/issues/575
+	projectPrefix := ""
+	if conn.ProjectID != "" {
+		projectPrefix = g.F("`%s`.", conn.ProjectID)
+	}
 
 	sqlTempl := `
 	delete from {tgt_table} tgt
 	where exists (
 			select 1
-			from {src_table} src
+			from {project_prefix}{src_table} src
 			where {src_tgt_pk_equal}
 	)
 	;
@@ -1096,6 +1089,7 @@ func (conn *BigQueryConn) GenerateUpsertSQL(srcTable string, tgtTable string, pk
 		sqlTempl,
 		"src_table", srcTable,
 		"tgt_table", tgtTable,
+		"project_prefix", projectPrefix,
 		"src_tgt_pk_equal", upsertMap["src_tgt_pk_equal"],
 		"set_fields", upsertMap["set_fields"],
 		"insert_fields", upsertMap["insert_fields"],
