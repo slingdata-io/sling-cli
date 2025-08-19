80a6224daa239ebbf598587b82749e85d76a21c9
Fritz Larco <flarco@live.com>
2025-08-19 16:19:17 -0300

    fix: increase timeout for TestSuiteDatabase to 35 minutes

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ cd cmd/sling
 go test -v -run 'TestReplicationDefaults'
 # SLING_PROCESS_BW=false go test -v -run 'TestSuiteDatabaseClickhouse' # gives issues when running in parallel
 go test -v -parallel 3 -run 'TestSuiteFile'
-SKIP_CLICKHOUSE=TRUE go test -v -parallel 4 -timeout 25m -run TestSuiteDatabase
+SKIP_CLICKHOUSE=TRUE go test -v -parallel 4 -timeout 35m -run TestSuiteDatabase
 cd -
 
 cd core/sling
