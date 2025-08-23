bb8a290f231dc8190b84f82c45eb08d77d5a00e3
Fritz Larco <flarco@live.com>
2025-08-23 09:51:24 -0300

    fix: update TimeType mapping to use TimeFieldType in BigQuery schema

--- core/dbio/database/database_bigquery.go
+++ core/dbio/database/database_bigquery.go
@@ -529,7 +529,8 @@ func getBqSchema(columns iop.Columns) (schema bigquery.Schema) {
 		// https://stackoverflow.com/questions/55904464/big-query-does-now-cast-automatically-long-decimal-values-to-numeric-when-runni
 		iop.DecimalType: bigquery.NumericFieldType,
 		// "decimal":   bigquery.FloatFieldType,
-		iop.TimeType:       bigquery.StringFieldType,
+		iop.TimeType:       bigquery.TimeFieldType,
+		iop.TimezType:      bigquery.TimeFieldType,
 		iop.TimestampType:  bigquery.TimestampFieldType,
 		iop.TimestampzType: bigquery.TimestampFieldType,
 	}
