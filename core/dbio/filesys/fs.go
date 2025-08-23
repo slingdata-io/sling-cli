191f8b5e11aa83b824a817afd525163c6c56d039
Fritz Larco <flarco@live.com>
2025-08-23 20:34:51 -0300

    fix: simplify DuckDB extension name for clarity

--- core/dbio/filesys/fs.go
+++ core/dbio/filesys/fs.go
@@ -1268,7 +1268,7 @@ func WriteDataflowViaDuckDB(fs FileSysClient, df *iop.Dataflow, uri string) (bw 
 
 	props := g.MapToKVArr(fs.Props())
 	duck := iop.NewDuckDb(context.Background(), props...)
-	duck.AddExtension("arrow from community")
+	duck.AddExtension("arrow")
 	duckURI := duck.PrepareFsSecretAndURI(uri)
 
 	sp := iop.NewStreamProcessor()
