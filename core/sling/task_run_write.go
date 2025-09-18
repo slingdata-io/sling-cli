ac395c6a2f248e80ba788c525a3b0ec7e72e551a
Fritz Larco <flarco@live.com>
2025-09-18 08:30:08 -0300

    fix: improve record count validation for StarRocks when using bulk loading

--- core/sling/task_run_write.go
+++ core/sling/task_run_write.go
@@ -329,13 +329,18 @@ func (t *TaskExecution) WriteToDb(cfg *Config, df *iop.Dataflow, tgtConn databas
 		if cnt != cast.ToUint64(tCnt) {
 			switch tgtConn.GetType() {
 			case dbio.TypeDbStarRocks:
-			// starrocks count can lag behind due to StarRocks’ async compaction/visibility.
-			// see https://github.com/slingdata-io/sling-cli/issues/644
-			// there is also a direct response check in database_starrocks.go, which would have errored, so we're good.
+				if cast.ToBool(tgtConn.GetProp("use_bulk")) {
+					// starrocks count can lag behind due to StarRocks’ async compaction/visibility.
+					// see https://github.com/slingdata-io/sling-cli/issues/644
+					// there is also a direct response check in database_starrocks.go, which would have errored, so we're good.
+					goto skipCountCompare
+				}
+				fallthrough // if not bulk loaded, then do compare
 			default:
 				err = g.Error("inserted in temp table but table count (%d) != stream count (%d). Records missing/mismatch. Aborting", tCnt, cnt)
 				return 0, err
 			}
+		skipCountCompare:
 		} else if tCnt == 0 && len(sampleData.Rows) > 0 {
 			err = g.Error("Loaded 0 records while sample data has %d records. Exiting.", len(sampleData.Rows))
 			return 0, err
@@ -513,13 +518,18 @@ func (t *TaskExecution) writeToDbDirectly(cfg *Config, df *iop.Dataflow, tgtConn
 		if tCnt >= 0 && cnt != cast.ToUint64(tCnt) {
 			switch tgtConn.GetType() {
 			case dbio.TypeDbStarRocks:
-			// starrocks count can lag behind due to StarRocks’ async compaction/visibility.
-			// see https://github.com/slingdata-io/sling-cli/issues/644
-			// there is also a direct response check in database_starrocks.go, which would have errored, so we're good.
+				if cast.ToBool(tgtConn.GetProp("use_bulk")) {
+					// starrocks count can lag behind due to StarRocks’ async compaction/visibility.
+					// see https://github.com/slingdata-io/sling-cli/issues/644
+					// there is also a direct response check in database_starrocks.go, which would have errored, so we're good.
+					goto skipCountCompare
+				}
+				fallthrough // if not bulk loaded, then do compare
 			default:
 				err = g.Error("inserted in temp table but table count (%d) != stream count (%d). Records missing/mismatch. Aborting", tCnt, cnt)
 				return 0, err
 			}
+		skipCountCompare:
 		} else if tCnt == 0 && len(sampleData.Rows) > 0 {
 			err = g.Error("loaded 0 records while sample data has %d records. Exiting.", len(sampleData.Rows))
 			return 0, err
