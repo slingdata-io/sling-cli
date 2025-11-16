8ee08f3bb9d1e5174e8109b80535031591815d6d
Fritz Larco <flarco@live.com>
2025-11-16 13:12:00 -0300

    feat(python): add build and test workflow for sling-python integration

--- scripts/test.all.sh
+++ scripts/test.all.sh
@@ -5,6 +5,7 @@ cd cmd/sling
 rm -f && go build .
 cd -
 
+export SLING_BINARY="$PWD/cmd/sling/sling"
 export RUN_ALL=true
 
 echo 'TESTING CLI'
@@ -17,3 +18,8 @@ echo 'TESTING dbio'
 cd core/dbio
 bash scripts/test.sh
 cd - 
+
+cd ../sling-python/sling
+python -m pytest tests/tests.py -v
+SLING_USE_ARROW=false python -m pytest tests/test_sling_class.py -v
+SLING_USE_ARROW=true python -m pytest tests/test_sling_class.py -v
