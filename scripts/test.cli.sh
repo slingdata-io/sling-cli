039644c4524b06b5d92729df56c79517d63343ad
Fritz Larco <flarco@live.com>
2025-11-26 13:00:22 -0300

    refactor(tests): remove unnecessary copy of sling binary in test script

--- scripts/test.cli.sh
+++ scripts/test.cli.sh
@@ -6,5 +6,4 @@ export AWS_ACCESS_KEY_ID=''     # clear aws env so s3 doesn't use it
 export AWS_SECRET_ACCESS_KEY='' # clear aws env so s3 doesn't use it
 
 cd cmd/sling
-cp ../../sling .
-SLING_BIN=./sling go test -v -parallel 6 -run TestCLI -timeout 20m
+go test -v -run TestCLI -timeout 20m
